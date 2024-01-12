d64d343c6ec00acf67d055338c9d0041469011c9
Fahim Murshed <murshed4@gmail.com>
2024-01-13 01:31:53 +0600

    Add files via upload

---
+++ patterns/home.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Title: home
+ * Slug: the-canvas/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"0","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--80);padding-bottom:0;padding-left:var(--wp--preset--spacing--80)"><!-- wp:column {"verticalAlignment":"top","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:33.33%"><!-- wp:site-title {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"x-large"} /-->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":119,"width":"211px","height":"211px","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"unset"},"layout":{"selfStretch":"fit","flexSize":null},"border":{"color":"#eeeeee","width":"5px"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/potrait-image.jpg" alt="" class="has-border-color wp-image-119" style="border-color:#eeeeee;border-width:5px;object-fit:cover;width:211px;height:211px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"21px","style":{"layout":{"flexSize":"21px","selfStretch":"fixed"}}} -->
+<div style="height:21px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+
+<!-- wp:social-links {"openInNewTab":true,"size":"has-small-icon-size","align":"center","style":{"spacing":{"blockGap":{"top":"0","left":"5%"},"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"className":"is-style-default","layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap","orientation":"horizontal"}} -->
+<ul class="wp-block-social-links aligncenter has-small-icon-size is-style-default" style="padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+
+<!-- wp:social-link {"url":"#","service":"youtube"} /-->
+
+<!-- wp:social-link {"url":"#","service":"github"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:query {"queryId":31,"query":{"perPage":"100","pages":"100","offset":0,"postType":"page","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]},"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-query alignfull"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"0","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:post-template {"style":{"typography":{"textTransform":"none"},"spacing":{"blockGap":"0"}},"layout":{"type":"default"},"fontSize":"small"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"16px","bottom":"var:preset|spacing|30","right":"16px","left":"1px"}},"border":{"radius":"0px","top":{"color":"#e4e4e4","width":"1px"},"right":{"width":"0px","style":"none","radius":"0px"},"bottom":{"color":"#e4e4e4","width":"1px"},"left":{"width":"0px","style":"none","radius":"0px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"center"}} -->
+<div class="wp-block-group" style="border-radius:0px;border-top-color:#e4e4e4;border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-color:#e4e4e4;border-bottom-width:1px;border-left-style:none;border-left-width:0px;padding-top:16px;padding-right:16px;padding-bottom:var(--wp--preset--spacing--30);padding-left:1px"><!-- wp:post-title {"textAlign":"left","level":6,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fit"},"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.1","textTransform":"uppercase","fontSize":"20px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:group --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%","style":{"border":{"left":{"color":"#e5e5e5","width":"1px"}},"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","bottom":"0","top":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="border-left-color:#e5e5e5;border-left-width:1px;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30);flex-basis:66.66%"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:column {"style":{"border":{"width":"0px","style":"none"}}} -->
+<div class="wp-block-column" style="border-style:none;border-width:0px"><!-- wp:query {"queryId":31,"query":{"perPage":100,"pages":"1000","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-query alignfull"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-template {"style":{"typography":{"textTransform":"none"},"spacing":{"blockGap":"0"},"elements":{"link":{":hover":{"color":{"text":"var:preset|color|black"}},"color":{"text":"var:preset|color|vivid-cyan-blue"}}}},"layout":{"type":"default"},"fontSize":"medium"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"16px","bottom":"var:preset|spacing|30","right":"16px","left":"1px"}},"border":{"radius":"0px","bottom":{"color":"#d2d2d2","width":"1px"},"top":{"color":"#d2d2d2","width":"1px"},"right":[],"left":[]}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size" style="border-radius:0px;border-top-color:#d2d2d2;border-top-width:1px;border-bottom-color:#d2d2d2;border-bottom-width:1px;padding-top:16px;padding-right:16px;padding-bottom:var(--wp--preset--spacing--30);padding-left:1px"><!-- wp:post-title {"level":6,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fixed","flexSize":"380px"},"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.1","textTransform":"capitalize","fontSize":"22px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}}},"textColor":"contrast"} /-->
+
+<!-- wp:post-date {"textAlign":"right","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"layout":{"selfStretch":"fill","flexSize":null},"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"500"}}} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:group --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:template-part {"slug":"footer","theme":"the-canvas","area":"footer"} /-->
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== The Canvas ===
+Contributors: Rajuan Islam
+Requires at least: 6.0
+Tested up to: 6.3.1
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+Introducing The Canvas —a testament to simplicity and elegance. 
+It has meticulously crafted a clean and uncluttered design that emphasizes the beauty of the written word, 
+free from any distractions. The Canvas theme embodies a minimalist approach, favoring a serene and soothing
+aesthetic to provide an eye-soothing experience for readers. 
+A muted color palette and thoughtful use of whitespace ensure that the spotlight is firmly on the showcased
+content, allowing for undisturbed immersion. Navigating through the theme is intuitive and seamless, 
+making it effortless for visitors to engage with the content and fully appreciate the art of writing. 
+With this block theme - The Canvas honors the essence of minimalism, prioritizing the purity and 
+elegance of each written piece.
+
+== Changelog ==
+
+= 1.0.1 =
+* Fixed footer section unavailable issues
+* Replaced image tracking links with main links
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+The Canvas WordPress Theme, (C) 2023 Rajuan
+The Canvas is distributed under the terms of the GNU GPL.
+The Canvas is based on Twenty Twenty-Three (https://wordpress.org/themes/twentytwentythree), 
+(C) the WordPress team, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+Twenty Twenty-Three WordPress Theme, (C) 2022-2023 WordPress.org
+Twenty Twenty-Three is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+This theme (The Canvas) has the following third-party resources:
+
+Images:
+
+Home page potrait image: people_man_famous_portrait_todd_carey-1386940
+CC0 Public Domain. Free for personal and commercial use. No attribution required.
+Image Source: 
+https://pxhere.com/en/photo/1386940
+
+
+Fonts: 
+
+Source Serif Pro: Source Serif Pro is a serif typeface in the transitional style, 
+designed to complement the Source Sans Pro family. Source Serif is an active Open Source project. 
+These fonts are licensed under the Open Font License. 
+License link: https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+IBM Plex Mono: IBM Plex® is the corporate typeface for IBM worldwide and 
+an open-source project developed by the IBM Brand & Experience team (BX&D). 
+It is an active Open Source project. These fonts are licensed under the Open Font License. 
+License link: https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+DM Sans: DM Sans is a low-contrast geometric sans serif design, intended for use at smaller text sizes. 
+The DM Sans project was commissoned by Google from Colophon. 
+It is an active Open Source project. These fonts are licensed under the Open Font License. 
+License link: https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+Inter Font: Copyright (c) 2016-2020 The Inter Project Authors.
+License: SIL Open Font License, 1.1, https://opensource.org/licenses/OFL-1.1
+Source: https://rsms.me/inter/
+
